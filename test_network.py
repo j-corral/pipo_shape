eb33bef99b130b5dd039fbbfe3c4e5e77ef83e01
Jonathan C <johnlejardinnier@gmail.com>
2018-01-10 00:25:35 +0100

    feat(test_network): add silent mode

--- test_network.py
+++ test_network.py
@@ -6,24 +6,31 @@ import imutils
 import cv2
 import dumper
 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image")
+ap.add_argument("-s", "--silent", required=False, help="enable silent mode")
+args = vars(ap.parse_args())
+
+# check silent mode
+silent = False
+if args['silent']:
+	silent = True
+
+# load the image
+image = cv2.imread(args["image"])
+orig = image.copy()
 
 # load labels
-print("Loading labels.json...")
+if not silent:
+	print("Loading labels.json...")
 LIST_WORD = dumper.loadData('labels.json')
 
 if len(LIST_WORD) == 0:
 	raise Exception("No labels found into labels.json ! Did you trained the model ?")
 
-print(LIST_WORD)
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image")
-args = vars(ap.parse_args())
-
-# load the image
-image = cv2.imread(args["image"])
-orig = image.copy()
+if not silent:
+	print(LIST_WORD)
 
 # pre-process the image for classification
 image = cv2.resize(image, (28, 28))
@@ -32,14 +39,16 @@ image = img_to_array(image)
 image = np.expand_dims(image, axis=0)
 
 # load the trained convolutional neural network
-print("[INFO] loading network...")
+if not silent:
+	print("[INFO] loading network...")
 # model = load_model(args["model"])
 model = load_model("dataset.model")
 
 # classify the input image
 i = 0;
 for k in model.predict(image)[0]:
-	print(LIST_WORD[i]["name"] + " : " + str(k))
+	if not silent:
+		print(LIST_WORD[i]["name"] + " : " + str(k))
 	LIST_WORD[i]["variable"] = k
 	i = i + 1
 
@@ -52,10 +61,13 @@ for word in LIST_WORD:
 
 label = "{}: {:.2f}%".format(label, proba * 100)
 
-# draw the label on the image
-output = imutils.resize(orig, width=400)
-cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 255, 0), 2)
+if silent:
+	print (label)
+else:
+	# draw the label on the image
+	output = imutils.resize(orig, width=400)
+	cv2.putText(output, label, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX,0.7, (0, 255, 0), 2)
 
-# show the output image
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+	# show the output image
+	cv2.imshow("Output", output)
+	cv2.waitKey(0)
